f412dfbb9608a5827f5858872fe2f3e6e98045a5
Anders Bertelrud <anders@apple.com>
2021-12-22 12:00:52 -0800

    Change to using new plugin APIs specified in SE-0325.

--- Plugins/SwiftProtobufPlugin/plugin.swift
+++ Plugins/SwiftProtobufPlugin/plugin.swift
@@ -1,54 +1,61 @@
 import PackagePlugin
- 
+import Foundation
+
 @main
 struct SwiftProtobufPlugin: BuildToolPlugin {
-    
-    func createBuildCommands(context: TargetBuildContext) throws -> [Command] {
 
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
         // We will generate an invocation of `protoc` for each input file, passing
         // it the path of the `protoc-gen-swift` generator tool.
+
+        // We only generate commands for source targets.
+        guard let target = target as? SourceModuleTarget else { return [] }
+
+        // In this case we generate an invocation of `protoc` for each input file,
+        // passing it the path of the `protoc-gen-swift` generator tool.
         let protocPath = try context.tool(named: "protoc").path
         let protocGenSwiftPath = try context.tool(named: "protoc-gen-swift").path
+
+        // This example configures the commands to write to a "GeneratedSources"
+        // directory.
+        let genSourcesDir = context.pluginWorkDirectory.appending("GeneratedSources")
 
         // Construct the search paths for the .proto files, which can include any of the
         // targets in the dependency closure. Here we assume that the public ones are in
         // a "protos" directory, but this can be made arbitrarily complex.
-        var protoSearchPaths = [context.targetDirectory]
-        protoSearchPaths.append(contentsOf: context.dependencies.map { target in
-            target.targetDirectory.appending("protos")
-        })
+        var protoSearchPaths = [target.directory] + target.recursiveTargetDependencies.map {
+            $0.directory.appending("protos")
+        }
 
-        // Add the search path to the system proto files. This sample implementation assumes
-        // that they are located relative to the `protoc` compiler provided by the binary
+        // Add the search path to the system proto files. This implementation assumes
+        // that they're located relative to the `protoc` compiler provided by the binary
         // target, but real implementation could be more sophisticated.
         protoSearchPaths.append(protocPath.removingLastComponent().removingLastComponent().appending("include"))
 
-        print(protoSearchPaths)
-
         // Iterate over the .proto input files, creating a command for each.
-        let inputFiles = context.inputFiles.filter { $0.path.extension == "proto" }
-        return inputFiles.map { inputFile in
+        let inputFiles = target.sourceFiles.filter { $0.path.extension == "proto" }
+        return inputFiles.map { inputFile in            
             // Construct the `protoc` arguments.
-            var arguments = [
+            var protocArgs = [
                 "--plugin=protoc-gen-swift=\(protocGenSwiftPath)",
-                "--swift_out=\(context.pluginWorkDirectory)",
+                "--swift_out=\(genSourcesDir)",
                 // "--swift_opt=ProtoPathModuleMappings=\(moduleMappingFile)"
             ]
-            arguments.append(contentsOf: protoSearchPaths.flatMap { ["-I", "\($0)"] })
-            arguments.append("\(inputFile.path)")
-            
+            protocArgs.append(contentsOf: protoSearchPaths.flatMap { ["-I", "\($0)"] })
+            protocArgs.append("\(inputFile.path)")
+
             // The name of the output file is based on the name of the input file, in a way
             // that's determined by the protoc source generator plug-in we're using.
             let outputName = inputFile.path.stem + ".pb.swift"
-            let outputPath = context.pluginWorkDirectory.appending(outputName)
-            
+            let outputPath = genSourcesDir.appending(outputName)
+
             // Construct the command. Specifying the input and output paths lets the build
             // system know when to invoke the command. The output paths are passed on to
             // the rule engine in the build system.
             return .buildCommand(
                 displayName: "Generating \(outputName) from \(inputFile.path.lastComponent)",
                 executable: protocPath,
-                arguments: arguments,
+                arguments: protocArgs,
                 inputFiles: [protocGenSwiftPath, inputFile.path],
                 outputFiles: [outputPath])
         }
